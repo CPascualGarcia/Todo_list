b54d01c828d7a30a2690d59815057afb4f4ca0b9
Carlos Pascual García <127403709+CPascualGarcia@users.noreply.github.com>
2024-12-27 18:17:09 +0100

    New functions

--- sql_jr_repl/src/sql_jr_parser.rs
+++ sql_jr_repl/src/sql_jr_parser.rs
@@ -1,4 +1,7 @@
 use nom::{LocatedSpan,IResult};
+use nom::bytes::complete::take_while1;
+use nom::combinator::map;
+use nom::bytes::complete::tag_no_case;
 
 // Define the data type RawSpan for a span of a string
 // LocatedSpan represents a span of text with a memory location
@@ -31,8 +34,10 @@ pub trait Parse<'a>: Sized {
 // Using tag_no_case from nom_supreme since its error is nicer
 // ParserExt is mostly for adding `.context` on calls to identifier to say what kind of identifier we want
 use nom_supreme::{tag::complete::tag_no_case, ParserExt};
-// many other imports omitted
+use nom::error::context; // Provides extra information in case of error
 
+
+// Data types supported in the database
 #[derive(Debug,Clone,Eq,Hash,PartialEq,Serialize,Deserialize)]
 pub enum sqlTypeinfo {
     String,
@@ -44,7 +49,67 @@ impl <'a> Parse<'a> for sqlTypeinfo {
     fn parse(input: RawSpan<'a>) -> ParseResult<'a, Self> {
         // Using context will pull later a better error message
         context("Column Type",
-        
+            alt((
+                map(tag_no_case("string"), |_| Self::String),
+                map(tag_no_case("int"), |_| Self::Int),
+            )),
         )(input)
     }
 }
+
+// Struct where we store the information in a column
+#[derive(Debug,Clone,Eq,Hash,PartialEq,Serialize,Deserialize)]
+pub struct Column {
+    pub name: String,
+    pub type_info: sqlTypeinfo,
+}
+
+// Parse "<colName> <colType>"
+impl <'a> Parse<'a> for Column {
+    fn parse(input: RawSpan<'a>) -> ParseResult<'a, Self> {
+        // Using context will pull later a better error message
+        context("Create column",
+            map(
+                separated_pair(identifier.context("Column name"),multispace1, sqlTypeinfo::parse),
+                |(name, type_info)| Self {name, type_info},
+            ),
+        )(input)
+    }
+}
+
+pub(crate) fn comma_sep<I, O, E, F>(
+    f: F,
+) -> impl FnMut(I) -> IResult<I, Vec<O>, E>
+where
+{
+    separated_list1(tuple((multispace0, char(','), multispace0)), f)
+}
+
+// Struct of the table provided its columns
+#[derive(Debug,Clone,Eq,Hash,PartialEq,Serialize,Deserialize)]
+pub struct CreateStatement {
+    pub table: String,
+    pub columns: Vec<Column>,
+}
+
+// List of column definitions
+fn column_definitions(input: RawSpan<'_>) -> ParseResult<'_, Vec<Column>> {
+    context(
+        "Column definitions",
+        map(tuple((char('('), comma_sep(Column::parse), char(')'))), 
+        |(_, columns, _)| columns,
+        ),
+    )(input)
+}
+
+
+
+
+
+
+
+
+
+
+
+
