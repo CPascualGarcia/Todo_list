60beb72f970a5148bf6d6093c065a1b6a2042895
Carlos Pascual García <127403709+CPascualGarcia@users.noreply.github.com>
2025-01-02 12:20:11 +0100

    New functions + changes in tests

--- sql_jr_repl/src/lib.rs
+++ sql_jr_repl/src/lib.rs
@@ -72,36 +72,10 @@ pub fn db_setup(db_path: &str) -> Result<(), std::io::Error> {
     Ok(())
 }
 
-
-
-pub fn db_add(db_path: &str, x: usize) -> Result<(), std::io::Error> {
+pub fn db_writer(db_path: &str, buffer: &str, x: usize) -> Result<(), std::io::Error> {
     let conn = Connection::open_with_flags(db_path,
         OpenFlags::SQLITE_OPEN_READ_WRITE | OpenFlags::SQLITE_OPEN_CREATE).unwrap();
 
-    // Check that the entry does not exist
-    let mut stmt = conn.prepare("SELECT * FROM tasks WHERE id = ?1").unwrap();
-    let mut rows = stmt.query(&[&x]).unwrap();
-    if rows.next().unwrap().is_some() {
-        println!("Entry id already defined in database. Still overwrite? [y/n]");
-        loop {
-            let mut buffer = String::new();
-            std::io::stdin().read_line(&mut buffer).expect("Failed to read line");
-            if buffer.trim() == "y" {
-                break;
-            }
-            else if buffer.trim() == "n" {
-                return Ok(());
-            }
-            else {
-                println!("Still overwrite? [y/n]");
-            }
-        }
-    };
-    
-    println!("Provide new entry in database:");
-    let mut buffer = String::new();
-    std::io::stdin().read_line(&mut buffer).expect("Failed to read line");
-    
     // Insert rows into the table
     let mut stmt = conn.prepare(
         "INSERT OR REPLACE INTO tasks (id, task) VALUES (?1, ?2)").unwrap();
@@ -109,6 +83,67 @@ pub fn db_add(db_path: &str, x: usize) -> Result<(), std::io::Error> {
 
     Ok(())
 }
+
+pub fn db_add(db_path: &str, x: usize) -> Result<(), std::io::Error> {
+    println!("Provide content to be written:");
+    let mut buffer: String = String::new();
+    std::io::stdin().read_line(&mut buffer).expect("Failed to read line");
+    db_writer(&db_path, &buffer, x as usize)?;
+
+    Ok(())
+}
+
+
+// pub fn db_add(db_path: &str, x: usize) -> Result<(), std::io::Error> {
+//     let conn = Connection::open_with_flags(db_path,
+//         OpenFlags::SQLITE_OPEN_READ_WRITE | OpenFlags::SQLITE_OPEN_CREATE).unwrap();
+
+//     // Check that the entry does not exist
+//     let mut stmt = conn.prepare("SELECT * FROM tasks WHERE id = ?1").unwrap();
+//     let mut rows = stmt.query(&[&x]).unwrap();
+//     if rows.next().unwrap().is_some() {
+//         println!("Entry id already defined in database. Still overwrite? [y/n]");
+//         loop {
+//             let mut buffer = String::new();
+//             std::io::stdin().read_line(&mut buffer).expect("Failed to read line");
+//             if buffer.trim() == "y" {
+//                 break;
+//             }
+//             else if buffer.trim() == "n" {
+//                 return Ok(());
+//             }
+//             else {
+//                 println!("Still overwrite? [y/n]");
+//             }
+//         }
+//     };
+    
+//     println!("Provide new entry in database:");
+//     let mut buffer = String::new();
+//     std::io::stdin().read_line(&mut buffer).expect("Failed to read line");
+    
+//     // Insert rows into the table
+//     let mut stmt = conn.prepare(
+//         "INSERT OR REPLACE INTO tasks (id, task) VALUES (?1, ?2)").unwrap();
+//     stmt.execute((x, buffer)).unwrap();
+
+//     Ok(())
+// }
+
+
+pub fn db_verify(db_path: &str, x: usize) -> bool {
+    let conn = Connection::open_with_flags(db_path,
+        OpenFlags::SQLITE_OPEN_READ_WRITE | OpenFlags::SQLITE_OPEN_CREATE).unwrap();
+
+    // Check that the entry does not exist
+    let mut stmt = conn.prepare("SELECT * FROM tasks WHERE id = ?1").unwrap();
+    let mut rows = stmt.query(&[&x]).unwrap();
+    if rows.next().unwrap().is_some() {
+        return true}
+    else {return false};
+}
+
+
 
 pub fn db_remove(db_path: &str, x: usize) -> Result<(), std::io::Error> {
     let conn = Connection::open_with_flags(db_path,
@@ -146,20 +181,11 @@ fn test_db_setup() {
         (&index, &entry),
     ).unwrap();
 
-    {
-    // Prepare a query statement
-    let mut stmt = conn.prepare("SELECT * FROM tasks WHERE id = ?1").unwrap();
-    
-    // Query the database
-    let mut rows = stmt.query(&[&14]).unwrap();
+    // Assert that the db recorded the entry
+    assert_eq!(db_verify(db_path, 14),true);
 
-    let row = rows.next().unwrap().unwrap();
-    let id: i32 = row.get(0).unwrap();
-    let task: String = row.get(1).unwrap();
-
-    assert_eq!(id, 14);
-    assert_eq!(task, "mustard");
-    };
+    // Assert that some other entry does not exist
+    assert_eq!(db_verify(db_path, 15),false);
 
     // Close the database
     conn.close().unwrap();
@@ -167,6 +193,34 @@ fn test_db_setup() {
     // Erase database
     remove_file(db_path).unwrap();
 
+}
+
+
+#[test]
+fn test_db_writer() -> Result<(), Box<dyn std::error::Error>> {
+    let db_path: &str = "TodoList_test.db"; // Prepare the path to the database
+    db_setup(db_path).unwrap();             // Set database
+
+    // Write to the database
+    db_writer(db_path, "mustard", 14).unwrap();
+
+    // Verify that the contents were written
+    let conn = Connection::open_with_flags(db_path,
+        OpenFlags::SQLITE_OPEN_READ_WRITE | OpenFlags::SQLITE_OPEN_CREATE,
+    ).unwrap();
+
+    let mut stmt = conn.prepare("SELECT * FROM tasks WHERE id = ?1")?;
+    let mut rows = stmt.query(&[&14])?;
+
+    // Check that the entry was recorded
+    let row = rows.next().unwrap().unwrap();
+    let id: i32 = row.get(0).unwrap();
+    let task: String = row.get(1).unwrap();
+
+    assert_eq!(id, 14);
+    assert_eq!(task, "mustard");
+
+    Ok(())
 }
 
 #[test]
@@ -181,15 +235,19 @@ fn test_db_empty_entry() -> Result<(), Box<dyn std::error::Error>> {
     ).unwrap();
 
     // Add a couple of entries
-    conn.execute(
-        "INSERT OR REPLACE INTO tasks (id, task) VALUES (?1, ?2)",
-        (1, "toy"),
-    ).unwrap();
+    db_writer(db_path, "toy", 1).unwrap();
+    db_writer(db_path, "mustard", 2).unwrap();
 
-    conn.execute(
-        "INSERT OR REPLACE INTO tasks (id, task) VALUES (?1, ?2)",
-        (2, "mustard"),
-    ).unwrap();
+
+    // conn.execute(
+    //     "INSERT OR REPLACE INTO tasks (id, task) VALUES (?1, ?2)",
+    //     (1, "toy"),
+    // ).unwrap();
+
+    // conn.execute(
+    //     "INSERT OR REPLACE INTO tasks (id, task) VALUES (?1, ?2)",
+    //     (2, "mustard"),
+    // ).unwrap();
 
     {
     let mut stmt = conn.prepare("SELECT * FROM tasks WHERE id = ?1")?;
